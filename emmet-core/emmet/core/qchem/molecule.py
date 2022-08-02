b784fce48958c6e64675e6489c49914691815ab1
samblau <samblau1@gmail.com>
2022-08-02 13:09:50 -0700

    debugging attempt

--- emmet-core/emmet/core/qchem/molecule.py
+++ emmet-core/emmet/core/qchem/molecule.py
@@ -45,15 +45,12 @@ def evaluate_lot(
     else:
         lot_comp = lot.split("/")
 
-    print(lot_comp[0])
-    print(lot_comp[1])
-    print(lot_comp[2])
-    print(lot_comp[2].split("(")[0])
+    solvent_field = lot_comp[2].split("(")
 
     return (
         -1 * funct_scores.get(lot_comp[0], 0),
         -1 * basis_scores.get(lot_comp[1], 0),
-        -1 * solvent_scores.get(lot_comp[2].split("(")[0], 0),
+        -1 * solvent_scores.get(solvent_field[0], 0),
     )
 
 
